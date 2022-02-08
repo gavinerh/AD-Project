54ef9e958d43fadb7ce3783cf910c5c0bd0919a2
joanne <joanne@users.noreply.github.com>
2022-02-08 11:43:47 +0800

    Updated NewsController (WEB)- check for duplicate articles

--- AD_Project_Server/src/main/java/com/example/demo/controller/NewsController.java
+++ AD_Project_Server/src/main/java/com/example/demo/controller/NewsController.java
@@ -21,6 +21,7 @@ import com.example.demo.model.Articles;
 import com.example.demo.model.Category;
 import com.example.demo.model.NewsSet;
 import com.example.demo.model.User;
+import com.example.demo.service.ArticlesService;
 import com.example.demo.service.NewsService;
 import Enumerates.category;
 
@@ -31,33 +32,28 @@ import Enumerates.category;
 public class NewsController {
 	
 	@Autowired
-	ArticlesRepo arepo;
+	ArticlesService aService;
 	@Autowired
 	SourceRepo srepo;
 	@Autowired
 	CategoryRepo crepo;
 	
-//	@RequestMapping(value="/")
-//	public NewsSet HomePage() {
-//		return NewsService.getNewsHome("tech", null, null);
-//	}
-	
-	@GetMapping(value="/")
-	public ResponseEntity<List<Articles>> HomePage() {
-		//created results ns1
-		NewsSet ns1 = NewsService.getNewsHome("technology", null, null);
-		
-		//just testing db
-		List<Articles> alist = ns1.getArticles();
-		System.out.println(alist.get(1).getTitle());
-//		for(Articles art:alist) {
-//			//--> to extract sources if need be
-//			srepo.save(art.getSource());
-//			//--> to save bookmarked articles
-//			arepo.save(art);
-//		}
-		return new ResponseEntity<List<Articles>>(alist, HttpStatus.OK);
+	//For WEB TEMPORARY
+	@RequestMapping(value="/")
+	public List<Articles> HomePage() {
+		NewsSet ns = NewsService.getNewsHome("technology", null, null);
+		List<Articles> alist = ns.getArticles();
+		for(Articles art:alist) {
+			//check if articles exist in  DB
+			if(aService.findExistngArticle(art.getTitle(), art.getDescription())==null) {
+				srepo.save(art.getSource()); //save sources
+				aService.save(art); //save articles
+			}
+		}
+		return alist;
 	}
+	
+	//For ANDROID TEMPORARY
 	@GetMapping("/news")
 	public ResponseEntity<List<Articles>> newsPage() {
 		
@@ -68,7 +64,7 @@ public class NewsController {
 /////////////////////////////////////////////////////////////////////
 		
 		//Fetch News from database
-		alist = arepo.findAll();
+		alist = aService.findAll();
 		
 		System.out.println("Fetched Articles size: "+alist.size());
 		List<Articles> android = new ArrayList<>();
@@ -81,40 +77,17 @@ public class NewsController {
 		return new ResponseEntity<List<Articles>>(android, HttpStatus.OK);
 	}
 	
-//	@GetMapping("/{country}/{category}")
-//	public List<Articles> requestByCountryAndCategory(@PathVariable
-//			String country, @PathVariable String category) {
-//			NewsSet nsCC = NewsService.getNewsByCountryCategory(country, category);
-//			return nsCC.getArticles();
-//	}
-	
-	 
-//	@GetMapping(value="/{country}/{category}")
-//	public NewsSet requestByCountryAndCategory(@PathVariable String country,
-//			@PathVariable String category) throws ParseException, IOException {
-//		return NewsService.getNewsByCountryCategory(country, category, null);
-//	}
-	
-//	@GetMapping(value="/kw/{keyword}")
-//	public NewsSet requestByKeywords(@PathVariable String keyword) {
-//		return NewsService.getNewsByKeyword(keyword, null);
-//	}
-//	
-//	@GetMapping(value="/src/{source1}")
-//	public NewsSet requestBySource(@PathVariable String source1) {
-//		return NewsService.getNewsBySource(source1, null);
-//	}
 	private List<Articles> fetchNewsAPI() {
 		List<Articles> nlist = new ArrayList<>();
 		List<Category> cats = crepo.findAll();
 		for(Category s:cats) {
 			nlist.addAll(NewsService.getNewsByCountryCategory(s.getName(),null));
 		}
-		arepo.deleteAll();
+		aService.deleteAll();
 		srepo.deleteAll();
 		for(Articles ar:nlist) {
 			srepo.saveAndFlush(ar.getSource());
-			arepo.saveAndFlush(ar);
+			aService.saveAndFlush(ar);
 		}
 		return nlist;
 	}
