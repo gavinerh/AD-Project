186fafdd7852cd77bb5dd15965a2f5f2608e5ddf
gavin erh <gavinerh@gmail.com>
2022-02-13 09:04:06 +0800

    added old folders

--- ADproj_newsview/src/main/java/com/news/app/repo/SourceRepo.java
+++
@@ -1,9 +0,0 @@
-package com.news.app.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.news.app.model.Source;
-
-public interface SourceRepo extends JpaRepository<Source, Integer>{
-
-}
