6eab7e5b363a6789473bef0fb7d20114bb22f82b
Mingyu96 <leemingyu3@gmail.com>
2022-02-21 10:35:21 +0800

    Fixed Like,Disliked and bookmarked state remembering error

--- AD_Project_Server/src/main/java/com/example/demo/model/JsonModel/ReactJson.java
+++ AD_Project_Server/src/main/java/com/example/demo/model/JsonModel/ReactJson.java
@@ -1,58 +1,69 @@
 package com.example.demo.model.JsonModel;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.example.demo.model.Articles;
+import com.example.demo.model.Category;
+import com.example.demo.model.Comment;
+import com.example.demo.model.Source;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 // json for communication with react and if possible for android
-public class ReactJson {
-	private String imageUrl;
-	private String webpageUrl;
+public class ReactJson extends Articles {
+	
+	private boolean isliked;
+	private boolean isbookmarked;
+	private boolean isdisliked;
+	private Source source;	
+	private String author;
 	private String title;
 	private String description;
-	private String category;
-
-	public ReactJson(String imageUrl, String webpageUrl, String title, String description, String category) {
-		super();
-		this.imageUrl = imageUrl;
-		this.webpageUrl = webpageUrl;
-		this.title = title;
-		this.description = description;
-		this.category = category;
-	}
-
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
-
-	public String getWebpageUrl() {
-		return webpageUrl;
-	}
-
-	public void setWebpageUrl(String webpageUrl) {
-		this.webpageUrl = webpageUrl;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
+	private String url;
+	private String urlToImage;
+	private String publishedAt;
+	private String prettytime;
+	private String content;
+	private List<Comment> comments;	
+	private Category category;
+	private int id;
+	
+	
+	
 	@Override
 	public String toString() {
-		return "ReactJson [imageUrl=" + imageUrl + ", webpageUrl=" + webpageUrl + ", title=" + title + ", description="
+		return "ReactJson [imageUrl=" + urlToImage + ", webpageUrl=" + url + ", title=" + title + ", description="
 				+ description + "]";
+	}
+
+	public ReactJson(int id, Source source, String author, String title, String description, String url,
+			String urlToImage, String publishedAt, String prettytime, String content, List<Comment> comments,
+			boolean isliked, Category category, boolean isdisliked, boolean isbookmarked) {
+		super();
+		this.id = id;
+		this.source = source;
+		this.author = author;
+		this.title = title;
+		this.description = description;
+		this.url = url;
+		this.urlToImage = urlToImage;
+		this.publishedAt = publishedAt;
+		this.prettytime = prettytime;
+		this.content = content;
+		this.comments = comments;
+		this.isliked = isliked;
+		this.category = category;
+		this.isdisliked= isdisliked;
+		this.isbookmarked = isbookmarked;
 	}
 	
 	
